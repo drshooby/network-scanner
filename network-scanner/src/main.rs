3bc5aa785bbe725677856540550c58b0ce0c522a
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-01-01 14:23:56 -0800

    add: find out if an address is private

--- network-scanner/src/main.rs
+++ network-scanner/src/main.rs
@@ -4,7 +4,6 @@ use crate::utilities::ip_resolve;
 mod utilities;
 
 fn main() {
-    println!("Hello, world!");
     let my_ip = match ip_resolve::outward_ip() {
         Ok(ip) => ip,
         Err(e) => {
@@ -12,13 +11,21 @@ fn main() {
             exit(1);
         }
     };
-    
-    match ip_resolve::classify_ip(my_ip) {
-        ip_resolve::IpClass::A => println!("My IP is an A class IP"),
-        ip_resolve::IpClass::B => println!("My IP is an B class IP"),
-        ip_resolve::IpClass::C => println!("My IP is an C class IP"),
-        _ => println!("Unknown IP class"),
-    }
-    
-    println!("My IP is: {my_ip}");
+
+    let ip_class = match ip_resolve::classify_ip(my_ip) {
+        ip_resolve::IpClass::A => "A",
+        ip_resolve::IpClass::B => "B",
+        ip_resolve::IpClass::C => "C",
+        _ => "Unknown",
+    };
+
+    let is_private = match ip_resolve::is_private_ip(my_ip) {
+        Ok(is_private) => is_private,
+        Err(e) => {
+            println!("Error: {e}");
+            exit(1);
+        }
+    };
+
+    println!("My IP is: {my_ip}, class {ip_class}, is private: {is_private}");
 }
--- network-scanner/src/utilities/ip_resolve.rs
+++ network-scanner/src/utilities/ip_resolve.rs
@@ -37,3 +37,30 @@ pub(crate) fn classify_ip(ip: IpAddr) -> IpClass {
         _ => IpClass::Unknown,
     }
 }
+
+fn is_a_type_private(octets: Vec<u8>) -> bool {
+    octets[0] == 10
+}
+
+fn is_b_type_private(octets: Vec<u8>) -> bool {
+    octets[0] == 172 && (octets[1] >= 16 && octets[1] <= 31)
+}
+
+fn is_c_type_private(octets: Vec<u8>) -> bool {
+    octets[0] == 192 && octets[1] == 168
+}
+
+pub(crate) fn is_private_ip(ip: IpAddr) -> Result<bool, String> {
+    let ip_class: IpClass = classify_ip(ip);
+    let octets: Vec<u8> = ip.to_string()
+        .split('.')
+        .map(|v| v.parse::<u8>().map_err(|_| format!("Could not parse octet: {v}")))
+        .collect::<Result<Vec<u8>, _>>()?;
+    
+    match ip_class {
+        IpClass::A => Ok(is_a_type_private(octets)),
+        IpClass::B => Ok(is_b_type_private(octets)),
+        IpClass::C => Ok(is_c_type_private(octets)),
+        _ => Err(String::from("Unknown IP class")),
+    }
+}
