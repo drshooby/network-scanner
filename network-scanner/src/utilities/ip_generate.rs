db5ffca0f5f5eb7963a7cb0d35aee0f8aff4ccaf
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-01-01 21:04:14 -0800

    update: generate_ips only generates based on last octet

--- network-scanner/src/utilities/ip_generate.rs
+++ network-scanner/src/utilities/ip_generate.rs
@@ -3,46 +3,44 @@ use std::str::FromStr;
 use crate::utilities::ip_resolve::{get_ip_info, IpClass, IpInfo};
 use ipnet::{IpAddrRange, Ipv4AddrRange};
 
-fn get_a_class_range() -> (String, String) {
-    (String::from("0.0.0.0"), String::from("127.255.255.255"))
-}
-
-fn get_b_class_range() -> (String, String) {
-    (String::from("128.0.0.0"), String::from("191.255.255.255"))
-}
-
-fn get_c_class_range() -> (String, String) {
-    (String::from("192.0.0.0"), String::from("223.255.255.255"))
-}
-
-pub(crate) fn generate_ips(ip_info: IpInfo) -> Result<Vec<IpInfo>, String> {
-    
-    let (start, end) = match ip_info.class {
-        IpClass::A => get_a_class_range(),
-        IpClass::B => get_b_class_range(),
-        IpClass::C => get_c_class_range(),
-        _ => return Err(String::from("Unknown class")),
-    };
-
+fn get_ips_from_range(start: String, end: String) -> Vec<IpInfo> {
     let hosts = IpAddrRange::from(Ipv4AddrRange::new(
         Ipv4Addr::from_str(&start).unwrap(),
         Ipv4Addr::from_str(&end).unwrap(),
     ));
-
     let mut ips = Vec::new();
-
-    let mut count = 0;
     for address in hosts {
-        if count> 100 {
-            break;
-        }
         let next_ip = match get_ip_info(address) {
             Ok(ip) => ip,
             Err(_) => continue
         };
         ips.push(next_ip);
-        count += 1;
+    }
+    ips
+}
+
+pub(crate) fn generate_ips(ip_info: IpInfo) -> Result<Vec<IpInfo>, String> {
+    
+    let is_default_scan = match ip_info.class {
+        IpClass::A | IpClass::B => false,
+        IpClass::C => true,
+        _ => return Err(String::from("Unknown class")),
+    };
+
+    let base = ip_info.ip
+        .to_string()
+        .rsplitn(2, '.')
+        .last()
+        .unwrap_or("0.0.0")
+        .to_string();
+
+    if is_default_scan {
+        let ips = get_ips_from_range(
+            format!("{}.0", base),
+            format!("{}.255", base),
+        );
+        return Ok(ips);
     }
 
-    Ok(ips)
+    Ok(Vec::new())
 }
