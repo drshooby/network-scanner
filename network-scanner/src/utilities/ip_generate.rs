d8503c0b4b3b84f14f89ec6d91426efbf39cdcf8
David Shubov <116600844+drshooby@users.noreply.github.com>
2025-01-01 16:50:51 -0800

    update: switched to using Ipv4AddrRange

--- network-scanner/src/utilities/ip_generate.rs
+++ network-scanner/src/utilities/ip_generate.rs
@@ -1,39 +1,47 @@
-use std::net::{IpAddr, Ipv4Addr};
+use std::net::{Ipv4Addr};
+use std::str::FromStr;
 use crate::utilities::ip_resolve::{get_ip_info, IpClass, IpInfo};
+use ipnet::{IpAddrRange, Ipv4AddrRange};
 
-
-fn get_a_class_range() -> (u32, u32) {
-    (0, 2147483647)
+fn get_a_class_range() -> (String, String) {
+    (String::from("0.0.0.0"), String::from("127.255.255.255"))
 }
 
-fn get_b_class_range() -> (u32, u32) {
-    (2147483648, 3221225471)
+fn get_b_class_range() -> (String, String) {
+    (String::from("128.0.0.0"), String::from("191.255.255.255"))
 }
 
-fn get_c_class_range() -> (u32, u32) {
-    (3221225472, 3758096383)
+fn get_c_class_range() -> (String, String) {
+    (String::from("192.0.0.0"), String::from("223.255.255.255"))
 }
 
 pub(crate) fn generate_ips(ip_info: IpInfo) -> Result<Vec<IpInfo>, String> {
-
-    let class = ip_info.class;
-    let (start, end) = match class {
+    
+    let (start, end) = match ip_info.class {
         IpClass::A => get_a_class_range(),
         IpClass::B => get_b_class_range(),
         IpClass::C => get_c_class_range(),
         _ => return Err(String::from("Unknown class")),
     };
 
+    let hosts = IpAddrRange::from(Ipv4AddrRange::new(
+        Ipv4Addr::from_str(&start).unwrap(),
+        Ipv4Addr::from_str(&end).unwrap(),
+    ));
+
     let mut ips = Vec::new();
 
-    for i in start..=end {
-        let curr_ip = IpAddr::V4(Ipv4Addr::from(i));
-        println!("Generating ip: {}", curr_ip);
-        let next_ip = match get_ip_info(curr_ip) {
+    let mut count = 0;
+    for address in hosts {
+        if count> 100 {
+            break;
+        }
+        let next_ip = match get_ip_info(address) {
             Ok(ip) => ip,
             Err(_) => continue
         };
         ips.push(next_ip);
+        count += 1;
     }
 
     Ok(ips)
